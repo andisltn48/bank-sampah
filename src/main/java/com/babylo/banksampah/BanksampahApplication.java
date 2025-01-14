1155dd6129a5f3f492e7a56b9dd926a39615bfbc
andisltn48 <andiaril186@gmail.com>
2025-01-14 22:43:38 +0800

    feat: initial

---
+++ src/main/java/com/babylo/banksampah/BanksampahApplication.java
@@ -0,0 +1,13 @@
+package com.babylo.banksampah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanksampahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanksampahApplication.class, args);
+	}
+
+}
